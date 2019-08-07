c28a6c1d27679a16343736507c54e5452e3c5541
Marcelo Arthur <marcelobragalemos@gmail.com>
2019-08-07 16:21:03 -0300

    Init commit create-react-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      string: 'Welcome to my new onClick function created and executed by react.js'
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>{this.state.string}</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick = {() => this.setState({string: 'HEY THATS CHANGED SO NICELY'})}>Change Text</button>
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           <code>Hello Worldzinh0</code>
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   )
+// }
