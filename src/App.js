34e6ae33be42a04de6d033e09c857612eaab7c0f
Marcelo Arthur <marcelobragalemos@gmail.com>
2019-08-12 17:00:17 -0300

    search-box updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 import './App.css';
 
 //creating the app class
@@ -24,14 +25,18 @@ class App extends Component {
   
   //rendering the things
   render() {
+    const { monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter(monster => 
+        monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+      )
+
     return (
       <div className="App">
-        <input 
-          type='search' 
-          placeholder='Search your contacts' 
-          onChange={e => this.setState({ searchField: e.target.value})}
+        <SearchBox
+          placeholder='Search Monsters'
+          handleChange={e => this.setState({ searchField: e.target.value })}
         />
-        <CardList monsters={this.state.monsters} /> 
+        <CardList monsters={filteredMonsters} /> 
     </div>
     )
   }
